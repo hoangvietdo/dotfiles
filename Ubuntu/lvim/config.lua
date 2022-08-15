a722b0b4eebd1c59c3bd051138a319c3d4a76d01
hoangvietdo <hoangvietdo.bk@gmail.com>
2022-08-15 20:59:03 +0900

    add plugins to config.lua

--- Ubuntu/lvim/config.lua
+++ Ubuntu/lvim/config.lua
@@ -60,7 +60,6 @@ vim.opt.autoindent     = true
 
 vim.cmd("autocmd InsertEnter * :set relativenumber")
 vim.cmd("autocmd InsertLeave * :set norelativenumber")
-vim.cmd("nnoremap <silent> <Enter> :nohlsearch<CR>")
 vim.cmd("nnoremap <C-J> <C-W><C-J>")
 vim.cmd("nnoremap <C-K> <C-W><C-K>")
 vim.cmd("nnoremap <C-L> <C-W><C-L>")
@@ -125,11 +124,55 @@ lvim.plugins = {
     "folke/lsp-colors.nvim",
     event = "BufRead"
   },
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+        hide_cursor = true, -- Hide cursor while scrolling
+        stop_eof = true, -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil, -- Default easing function
+        pre_hook = nil, -- Function to run before the scrolling animation starts
+        post_hook = nil, -- Function to run after the scrolling animation ends
+      })
+    end
+  },
+  {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
+  {
+    "metakirby5/codi.vim",
+    cmd = "Codi",
+  },
+  {
+    "ethanholz/nvim-lastplace",
+    event = "BufRead",
+    config = function()
+      require("nvim-lastplace").setup({
+        lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+        lastplace_ignore_filetype = {
+          "gitcommit", "gitrebase", "svn", "hgcommit",
+        },
+        lastplace_open_folds = true,
+      })
+    end,
+  },
   { "tpope/vim-surround" },
   { "matze/vim-move" },
   { "terryma/vim-multiple-cursors" },
   { "junegunn/vim-easy-align" },
   { "romgrk/barbar.nvim" },
   { "sainnhe/sonokai" },
-  { "Yggdroot/indentLine" }
+  { "Yggdroot/indentLine" },
+  { "tpope/vim-repeat" }
 }
