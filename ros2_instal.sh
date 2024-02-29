6f7d59a1a30eb8a854b206c3bda90a54944021a2
Viet <51826956+hoangvietdo@users.noreply.github.com>
2024-02-29 20:53:58 +0900

    Update ros2_instal.sh

--- ros2_instal.sh
+++ ros2_instal.sh
@@ -1,13 +1,14 @@
-sudo apt update && sudo apt install locales
+sudo apt update && sudo apt install locales -y
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository universe
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt upgrade
-sudo apt install ros-foxy-desktop python3-argcomplete
-sudo apt install ros-dev-tools
+sudo apt install ros-foxy-desktop python3-argcomplete -y
+sudo apt install ros-dev-tools -y
+echo "source /opt/ros/foxy/setup.zsh" >> ~/.zshrc
