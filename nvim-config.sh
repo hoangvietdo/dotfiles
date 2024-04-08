1ec91d6969ce424ebd509fafc50e32b6c32489dd
hoangvietdo <hoangvietdo.bk@gmail.com>
2024-04-08 12:28:47 +0900

    update nvim-config.sh

--- nvim-config.sh
+++ nvim-config.sh
@@ -11,7 +11,13 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	cd
 	sudo add-apt-repository ppa:neovim-ppa/unstable -y
 	sudo apt update
-	sudo apt install git make gcc ripgrep unzip xclip python3-pip neovim -y
+	sudo apt install git make gcc ripgrep unzip xclip curl ninja-build gettext python3-pip neovim npm -y
+	pip install -U pynvim
+	pip install 'python-lsp-server[all]' pyright
+	wget https://nodejs.org/dist/v14.15.4/node-v14.15.4-linux-x64.tar.xz
+	mkdir -p $HOME/tools
+	tar xvf node-v14.15.4-linux-x64.tar.xz --directory=$HOME/tools
+	npm install -g vim-language-server
 	git clone https://github.com/hoangvietdo/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 	nvim
 	echo "Finished!!!"
